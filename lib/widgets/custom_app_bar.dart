7f3a99133b880c9188d99ac3e89d217493a9c702
Dylan <dylan.vauthieu@epitech.eu>
2023-12-13 22:01:24 +0100

    [+] Add appbar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
 import 'package:netflix_clone/assets.dart';
 
 class CustomAppBar extends StatelessWidget {
@@ -7,11 +9,60 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 24.0,
+      ),
       color: Colors.orange,
-      child: Row(
-        children: [
-          Image.asset(Assets.netflixLogo0),
-        ]
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(Assets.netflixLogo0),
+            const SizedBox(width: 12.0),
+            Row(
+              children: [
+                _AppBarButton(
+                  title: 'TV Shows',
+                  onTaps: () => print('TV Shows')
+                ),
+                _AppBarButton(
+                  title: 'Movies',
+                  onTaps: () => print('Movies')
+                ),
+                _AppBarButton(
+                  title: 'My list',
+                  onTaps: () => print('My list')
+                ),
+              ],
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+class _AppBarButton extends StatelessWidget {
+  final String title;
+  final Function() onTaps;
+
+  const _AppBarButton({
+    Key? key,
+    required this.title,
+    required this.onTaps,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTaps,
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w600,
+        ),
       ),
     );
   }
